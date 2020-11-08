e838e2ea53461f20df345ffd561897d9b76625d5
Christophe Lambin <christophe.lambin@gmail.com>
2020-11-08 17:24:25 +0100

    working version

--- grafana_exporter.py
+++ grafana_exporter.py
@@ -1,8 +1,27 @@
+import os
+from pathlib import Path
 import logging
 import requests
 from slugify import slugify
 import json
 import yaml
+from yaml.representer import SafeRepresenter
+
+
+class LiteralStr(str):
+    pass
+
+
+def change_style(style, representer):
+    def new_representer(dumper, data):
+        scalar = representer(dumper, data)
+        scalar.style = style
+        return scalar
+    return new_representer
+
+
+represent_literal_str = change_style('|', SafeRepresenter.represent_str)
+yaml.add_representer(LiteralStr, represent_literal_str)
 
 
 class GrafanaDBExporter:
@@ -14,54 +33,127 @@ class GrafanaDBExporter:
         response = requests.get(f'{self._url}{endpoint}', headers=self._headers)
         if response.status_code != 200:
             logging.warning(f'Call to {endpoint} failed: {response.status_code} - {response.reason}')
-            return None
+            return dict()
         return response.json()
 
-    def export_datasources(self, filename):
+    @staticmethod
+    def _build_configmap(files, name, namespace, indent=2):
+        configmap = {
+            'kind': 'ConfigMap',
+            'apiVersion': 'v1',
+            'metadata': {
+                'name': name,
+                'namespace': namespace,
+            },
+            'data': dict()
+        }
+        for filename, content in files.items():
+            configmap['data'][filename] = LiteralStr(yaml.dump(content, indent=indent))
+        return configmap
+
+    @staticmethod
+    def _save_yaml(path, content, indent=2):
+        Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
+        with open(path, 'w') as f:
+            f.write(yaml.dump(content, indent=indent))
+            f.close()
+
+    @staticmethod
+    def _save_json(path, content, indent=4):
+        Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
+        with open(path, 'w') as f:
+            f.write(json.dumps(content, indent=indent))
+            f.close()
+
+    def _get_datasources(self):
         datasources = {
-            'apiVersion': '1',
+            'apiVersion': 1,
             'datasources': []
         }
         for source in self._call('/api/datasources'):
             if (datasource := self._call(f'/api/datasources/{source["id"]}')) is not None:
                 datasources['datasources'].append(datasource)
-        with open(filename,'w') as f:
-            f.write(yaml.dump(datasources))
+        return datasources
 
-    def export_dashboards(self, directory):
-        for dashboard in self._call('/api/search?query=&/'):
+    def export_datasources(self, directory):
+        GrafanaDBExporter._save_yaml(os.path.join(directory, 'datasources.yml'), self._get_datasources())
+
+    def export_datasources_configmap(self, directory, name='grafana-provisioning-datasources', namespace='monitoring'):
+        GrafanaDBExporter._save_yaml(
+            os.path.join(directory, f'{name}.yml'),
+            GrafanaDBExporter._build_configmap({'datasources.yml': self._get_datasources()}, name, namespace)
+        )
+
+    def _get_dashboard_folders(self, folders):
+        ids = dict()
+        for folder in self._call('/api/folders'):
+            if folder['title'] in folders:
+                ids[folder['title']] = folder['id']
+        if 'General' in folders:
+            ids['General'] = 0
+        return ids
+
+    def _get_dashboards(self, folderid):
+        dashboards = {}
+        query = f'folderIds={folderid}&type=dash-db'
+        for dashboard in self._call(f'/api/search?{query}'):
             if (export := self._call(f'/api/dashboards/uid/{dashboard["uid"]}')) is not None:
-                with open(f'{directory}/{slugify(dashboard["title"])}.json', 'w') as f:
-                    f.write(json.dumps(export['dashboard'], indent=4))
+                if not export['meta']['isFolder']:
+                    dashboards[slugify(dashboard["title"])] = export['dashboard']
+        return dashboards
 
-    def get_dashboards_old(self):
-        url = f'{self._url}/api/search?query=&/'
-        response = requests.get(url, headers=self._headers)
-        if response.status_code != 200:
-            logging.warning(f'Failed to get dashboards: {response.status_code} - {response.reason}')
-            return None
-        return [dashboard['uri'] for dashboard in response.json()]
+    @staticmethod
+    def _get_dashboard_provisioning(name):
+        return {
+            'apiVersion': 1,
+            'providers': [{
+                'name': name,
+                'orgId': 1,
+                'folder': '',
+                'disableDeletion': False,
+                'updataIntervalSeconds': 10,
+                'allowUiUpdates': True,
+                'options': {
+                    'path': '/var/lib/grafana/dashboards',
+                    'foldersFromFilesStructure': True
+                }
+            }]
+        }
 
-    def export_dashboards_old(self):
-        for dashboard in self.get_dashboards_old():
-            url = f'{self._url}/api/dashboards/{dashboard}'
-            response = requests.get(url, headers=self._headers)
-            if response.status_code != 200:
-                logging.warning(f'Failed to get dashboard: {response.status_code} - {response.reason}')
-                continue
-            logging.debug(response.json())
-            if response.json()['meta']['provisioned']:
-                logging.debug(f'{dashboard} is a provisioned dashboard. Skipping')
-                continue
-            export = response.json()['dashboard']
-            logging.info(f'{dashboard}: {export}')
-            with open('dashboards/' + slugify(dashboard) + '.json', 'w') as f:
-                f.write(json.dumps(export))
+    def export_dashboards(self, directory, folders):
+        GrafanaDBExporter._save_yaml(
+            os.path.join(directory, 'dashboards.yml'),
+            GrafanaDBExporter._get_dashboard_provisioning('my dashboards')
+        )
+        for foldername, folderid in self._get_dashboard_folders(folders).items():
+            for title, dashboard in self._get_dashboards(folderid).items():
+                logging.info(f'Exporting {foldername}/{title}')
+                GrafanaDBExporter._save_json(os.path.join(directory, foldername, f'{title}.json'), dashboard)
+
+    def export_dashboards_configmap(self, directory, folders, name='grafana-provisioning-dashboards', namespace='monitoring'):
+        configmap = GrafanaDBExporter._build_configmap(
+            {'dashboards.yml': GrafanaDBExporter._get_dashboard_provisioning('my dashboards')},
+            name, namespace
+        )
+        GrafanaDBExporter._save_yaml(os.path.join(directory, 'grafana-provisioning-dashboards.yml'), configmap)
+
+        for foldername, folderid in self._get_dashboard_folders(folders).items():
+            configmap = GrafanaDBExporter._build_configmap({}, f'grafana-dashboards-{slugify(foldername)}', namespace)
+            for title, dashboard in self._get_dashboards(folderid).items():
+                logging.info(f'Exporting (configmap) {foldername}/{title}')
+                configmap['data'][f'{title}.json'] = LiteralStr(json.dumps(dashboard, indent=2))
+            GrafanaDBExporter._save_yaml(
+                os.path.join(directory, f'{slugify(configmap["metadata"]["name"])}.yml'),
+                configmap
+            )
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
     api_key = 'eyJrIjoiRXVEcTNFQm0zb0JXMWNGNnhEOHJjandFOFptTTlzNUwiLCJuIjoiZ3JhZmFuYV9leHBvcnRlciIsImlkIjoxfQ=='
     exporter = GrafanaDBExporter('http://grafana.192.168.0.11.nip.io', api_key)
-    exporter.export_datasources('datasources/datasources.yml')
-    exporter.export_dashboards('dashboards')
+    exporter.export_datasources('datasources')
+    exporter.export_dashboards('dashboards', ['General', 'system', 'media', 'covid'])
+    exporter.export_datasources_configmap('k8s')
+    exporter.export_dashboards_configmap('k8s', ['General', 'system', 'media', 'covid'])
