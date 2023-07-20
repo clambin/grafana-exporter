3f9ad2ae2426f615ab1893dcc503293f29df2fd9
clambin <christophe.lambin@gmail.com>
2023-07-20 15:33:58 +0200

    chore(deps): bump golang.org/x/exp to latest

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.16.0
 	github.com/stretchr/testify v1.8.4
-	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -11,7 +11,7 @@ var (
 	configFilename string
 	RootCmd        = &cobra.Command{
 		Use:   "grafana-exporter",
-		Short: "exports Grafana dashboards, dashboard provisioning & datasource provisioning",
+		Short: "exports Grafana dashboards & datasources",
 	}
 )
 
@@ -23,14 +23,14 @@ func init() {
 func initArgs() {
 	RootCmd.Version = version.BuildVersion
 
-	RootCmd.PersistentFlags().StringVarP(&configFilename, "config", "c", "", "Configuration file")
+	RootCmd.Flags().StringVarP(&configFilename, "config", "c", "", "Configuration file")
 	RootCmd.PersistentFlags().BoolP("debug", "d", false, "Log debug messages")
 	RootCmd.PersistentFlags().Bool("direct", false, "Write config files directory (default: write as k8s config maps)")
 	RootCmd.PersistentFlags().StringP("namespace", "n", "default", "Namespace for K8s config maps")
 	RootCmd.PersistentFlags().StringP("grafana.url", "u", "", "Grafana URL")
 	RootCmd.PersistentFlags().StringP("grafana.token", "t", "", "Grafana API token (must have admin access)")
 	RootCmd.PersistentFlags().StringP("out", "o", "", "Output directory")
-	RootCmd.PersistentFlags().StringP("mode", "m", "", "Output mode (local/git)")
+	RootCmd.PersistentFlags().StringP("mode", "m", "local", "Output mode (local/git)")
 
 	_ = viper.BindPFlag("debug", RootCmd.PersistentFlags().Lookup("debug"))
 	_ = viper.BindPFlag("direct", RootCmd.PersistentFlags().Lookup("direct"))
@@ -40,10 +40,10 @@ func initArgs() {
 	_ = viper.BindPFlag("out", RootCmd.PersistentFlags().Lookup("out"))
 	_ = viper.BindPFlag("mode", RootCmd.PersistentFlags().Lookup("mode"))
 
-	dashboardsCmd.PersistentFlags().StringP("folders", "f", "", "Dashboard folders to export")
-	_ = viper.BindPFlag("folders", RootCmd.PersistentFlags().Lookup("folders"))
-	RootCmd.AddCommand(dashboardsCmd)
+	dashboardsCmd.Flags().StringP("folders", "f", "", "Dashboard folders to export")
+	_ = viper.BindPFlag("folders", dashboardsCmd.Flags().Lookup("folders"))
 
+	RootCmd.AddCommand(dashboardsCmd)
 	RootCmd.AddCommand(datasourcesCmd)
 }
 
